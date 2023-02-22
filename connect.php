62f50b723d556a7492c8e5f453178543992fc3fa
VenkatMalla7 <126048596+VenkatMalla7@users.noreply.github.com>
2023-02-22 00:19:40 -0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    // $con=new mysqli('localhost','root','','myref');
+    $con = new mysqli('localhost', 'root', '', 'myref','3325')
+    // if(!$con)
+    // {
+    //     die(mysqli_error($con));
+    // }
+
+
+    // $con->close();
